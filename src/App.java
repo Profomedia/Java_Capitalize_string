bd2a4e514bc07c8a07478beeba1254de6e5150bc
Profo <profogitrepos@gmail.com>
2024-08-13 16:13:51 +0300

    fixed the while loop problem

--- src/App.java
+++ src/App.java
@@ -8,20 +8,43 @@ public class App {
         System.out.print("Enter name or city: ");
         word = scanner.nextLine().trim();
         
-        while(word.isBlank() || word.length() < 3){
-            System.out.print("Min length of char should be at least 3: ");
-            word = scanner.nextLine().trim();
-            
-            // TODO  split the conditions to prompt whether the input is blank
-        }
+        // run the check limit
+        checkLimits(word);
+
+        // close the scanner
         scanner.close();
+
+        // print the capitalized out put
         System.out.println(cap(word));
     }
 
+    static void checkLimits(String word){
+        // get input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // check if input is empty
+        while(word.isEmpty()){
+            System.out.print("Input cannot be empty: ");
+            word = scanner.nextLine().trim();
+        }
+        // check length
+        while(word.length() < 3){
+            System.out.print("Characters must be at least 3, try again: ");
+            word = scanner.nextLine().trim();
+
+            // this line checks if the word is both empty and less
+            if(word.isEmpty()){
+                System.out.print("Input cannot be empty and ");
+            }
+        }
+        scanner.close();
+    }
+
+    // capitalize the string
     public static String cap(String s){
         // extract the first letter and convert to uppercase
         String firstLetter = s.substring(0, 1).toUpperCase();
-
+        
         // slice the first character of the string
         String strNoHead = s.substring(1, s.length());
 
